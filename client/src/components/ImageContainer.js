35b2088bfce01e54f8c68b9e170dc41679d87e3e
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-03-23 15:45:37 -0400

    Got initial functionality working of sliding through images

--- client/src/components/ImageContainer.js
+++ client/src/components/ImageContainer.js
@@ -4,31 +4,48 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 import { fetchImages } from '../actions/actions';
+import Image from './Image';
 
 class ImagesContainer extends Component {
   state = {
     category: 'cats',
+    index: 0,
   };
   componentDidMount() {
     this.props.fetchImages(this.state.category);
   }
 
-  handleCategory(type) {
+  handleCategory = type => {
     return e => {
+      this.props.fetchImages(type);
       this.setState({ category: type });
     };
-  }
+  };
+
+  handleImage = action => {
+    return e => {
+      let index = this.state.index;
+      const imageCount = this.props.images.length;
+      if (action === 'next') {
+        index = index + 1 > imageCount - 1 ? 0 : index + 1;
+      }
+      if (action === 'prev') {
+        index = index - 1 < 0 ? imageCount - 1 : index - 1;
+      }
+      this.setState({ index });
+    };
+  };
 
   render() {
-    const postItems = this.props.images.map(image => (
-      <img src={image} alt="animal" />
-    ));
     return (
       <div>
         <h1>Images</h1>
+        <p>{this.state.index}</p>
         <p onClick={this.handleCategory('cats')}>Cats</p>
         <p onClick={this.handleCategory('sharks')}>Sharks</p>
-        {postItems}
+        <i class="fas fa-chevron-left" onClick={this.handleImage('prev')} />
+        <i class="fas fa-chevron-right" onClick={this.handleImage('next')} />
+        <img src={this.props.images[this.state.index]} alt="" srcset="" />
       </div>
     );
   }
