edbac3e93aa9dacd84fc0b9dae91f51264dddf02
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-03-24 21:00:24 -0400

    Added snapshot tests and comments

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 export const FETCH_IMAGES = 'FETCH_IMAGES';
 
+//Breaking down function to allow injection during testing
 const getImagesByCategory = category =>
   fetch(`/images/${category}`).then(res => res.json());
 
@@ -7,7 +8,7 @@ export const fetchImagesBase = fetcher => category => dispatch => {
   dispatch({
     type: `${FETCH_IMAGES}_REQUEST`,
   });
-  return fetcher(category)
+  return fetcher(category) //Only returning so test can be validated that dispatch does indeed get called twice on SUCCESS
     .then(imagesObj => {
       dispatch({
         type: `${FETCH_IMAGES}_SUCCESS`,
--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -1,19 +1,19 @@
 import { FETCH_IMAGES } from '../actions';
 
-export const initialState = Object.freeze({
+export const initialState = {
   user: {},
   images: [],
   categories: ['cats', 'sharks'],
   isLoading: false,
-});
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case `${FETCH_IMAGES}_REQUEST`:
+    case `${FETCH_IMAGES}_REQUEST`: //Handles the loading state
       return { ...state, images: [], isLoading: true };
     case `${FETCH_IMAGES}_SUCCESS`:
       return { ...state, images: action.payload, isLoading: false };
-    // TODO: Error handling
+    // TODO: Error handling - Unable to complete at this time
     case `${FETCH_IMAGES}_FAILURE`:
       return { ...state, isLoading: false };
     default:
